62324c5d6fdc4601edf7d99ecb9e2ce9ed5237c5
Filippo Catellani <74590518+officialBurtaxz@users.noreply.github.com>
2024-03-14 12:32:40 +0100

    Add files via upload

--- FindWords/Global.gd
+++ FindWords/Global.gd
@@ -1,12 +1,18 @@
 extends Node
 
 var resGrid = 0
+var words = []
 
+const MAX5 = 2
+const MAX8 = 4
+const MAX10 = 6
+const MAX12 = 8
+const MAX15 = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
