083c28d5e33c92203ed59cb58438380e47b9ac85
Filippo Catellani <74590518+officialBurtaxz@users.noreply.github.com>
2024-03-14 12:32:24 +0100

    Add files via upload

--- FindWords/Menu/Menu.gd
+++ FindWords/Menu/Menu.gd
@@ -1,19 +1,20 @@
 extends Node2D
 
 @onready var drop_down_menu = $CanvasLayer/OptionButton
-
+var cont = 0
+var selected = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_items()
 
 
 func add_items():
+	drop_down_menu.add_item("-", 5)
 	drop_down_menu.add_item("5x5", 5)
 	drop_down_menu.add_item("8X8", 8)
 	drop_down_menu.add_item("10x10", 10)
 	drop_down_menu.add_item("12x12", 12)
 	drop_down_menu.add_item("15x15", 15)
-	drop_down_menu.add_item("20x20", 20)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,24 +23,46 @@ func _process(delta):
 
 func _on_option_button_item_selected(index):
 	match index:
-		0:
-			Global.resGrid = 5
 		1:
-			Global.resGrid = 8
+			fill_Info(5, Global.MAX5)
 		2:
-			Global.resGrid = 10
+			fill_Info(8, Global.MAX8)
 		3:
-			Global.resGrid = 12
+			fill_Info(10, Global.MAX10)
 		4:
-			Global.resGrid = 15
+			fill_Info(12, Global.MAX12)
 		5:
-			Global.resGrid = 20
+			fill_Info(15, Global.MAX15)
 		_:
 			push_error("ERROR")
 			
-	$Button.show()
+	$CanvasLayer/OptionButton.disabled = true
+	$CanvasLayerParole.show()
 	
 
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Grid/Grid.tscn")
+
+
+func _on_button_submit_pressed():
+	print(cont, selected)
+	if cont != selected:
+		var new_word = str($CanvasLayerParole/Label_PAROLE/TextEdit.text)
+		if len(new_word) <= Global.resGrid:
+			Global.words.append(new_word)
+			$CanvasLayerParole/Label_PAROLE/TextEdit.text = ""
+			$CanvasLayerParole/Label_PAROLE.text = "\n RIMANENTI: " + str((selected - 1 - cont)) + "\nLUNGHEZZA MAX PAROLA: " + str(Global.resGrid)
+			print(Global.words)
+			if selected - 1 == cont:
+				$CanvasLayerParole.hide()
+				$Button.show()
+			cont += 1
+			
+
+
+func fill_Info(grid_dim, max_word):
+	Global.resGrid = grid_dim
+	$CanvasLayerParole/Label_PAROLE.text += "\n INSERIRE " + str(max_word) + " PAROLE\nLUNGHEZZA MAX PAROLA: " + str(Global.resGrid) 
+	selected = max_word
+	print(selected)
